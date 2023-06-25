46a923cb9233534a36748585fcc364357ce0ec39
Pal Bijewar <pal.bijewar@gmail.com>
2023-06-25 15:55:33 +0530

    email feature added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_ng24ag5',
+        'template_3kbvmgq',
         {
           from_name: form.name,
           to_name: "Pal Bijewar",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "pal.bijewar@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '73ou3IKRF1gzsNzsx'
       )
       .then(
         () => {
